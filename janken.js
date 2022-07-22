32f069805542f2484d4501d2a572c6273ad7b3a3
Robert Chen <bergintonatcapital@gmail.com>
2022-07-22 01:08:44 -0400

    Add game function for 5 rounds to determine winner overall

--- janken.js
+++ janken.js
@@ -8,7 +8,7 @@ function getComputerChoice() {
 
 // ask user for choice prompt
 function getPlayerChoice() {
-    let choice = prompt("Rock, paper, or scissor?").toLowerCase()
+    let choice = prompt("Dr.Evel & his minions has challenge you to a battle! Pick your weapon: rock, paper, or scissor?").toLowerCase()
 
     while(choice != "rock" && choice != "paper" && choice != "scissor") {
         alert("Not a valid choice, try again.")
@@ -17,33 +17,39 @@ function getPlayerChoice() {
     return choice;
 }
 
-const computerChoice = getComputerChoice();
-const playerChoice = getPlayerChoice();
+let player_score = 0;
+let computer_score = 0;
 
 // decides the winner base on the two choices
 function playRound(computerChoice, playerChoice) {
-    console.log("computer chooses: " + computerChoice);
+    console.log("Dr.Evel chooses: " + computerChoice);
     switch (computerChoice) {
         case 'rock':
             if (playerChoice == 'rock') {
                 return "It's a Tie!";
             } else if (playerChoice == 'paper')  {
+                player_score++;
                 return "You Win! Paper beats Rock";
             } else {
+                computer_score++;
                 return "You Lose! Rock beats Scissor";
             }
         case 'paper':
             if (playerChoice == 'rock') {
+                computer_score++;
                 return "You Lose! Paper beats Rock";
             } else if (playerChoice == 'paper')  {
                 return "It's a Tie!";
             } else {
+                player_score++;
                 return "You Win! Scissor beats Paper";
             }
         case 'scissor':
             if (playerChoice == 'rock') {
+                player_score++;
                 return "You Win! Rock beats Scissor";
             } else if (playerChoice == 'paper')  {
+                computer_score++;
                 return "You Lose! Scissor beats Paper";
             } else {
                 return "It's a Tie!";
@@ -53,4 +59,24 @@ function playRound(computerChoice, playerChoice) {
     }
 }
 
-console.log(playRound(computerChoice, playerChoice));
+//Plays 5 rounds & determine the winner
+function game() {
+    for(let i = 0; i < 5; i++) {
+        console.log("~SCORE~ You: " + player_score +  " Dr.Evel: " + computer_score);
+        console.log(playRound(getComputerChoice(), getPlayerChoice()));
+    }
+
+    if (player_score < computer_score) {
+        console.log("Dr.Evel & his minions have won! We'll get em next time!");
+    } else if (player_score > computer_score) {
+        console.log("You triumph over Dr.Evel & his minions. Huzzah!!");
+    } else {
+        console.log("Stalemate! A worthy opponent indeed, your next battle will be legendary!");
+    }
+}
+
+//start game!
+game();
+
+
+
