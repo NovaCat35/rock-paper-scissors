fafbcf211a3ecef6a353bbcc5629766da91a1baf
Robert Chen <bergintonatcapital@gmail.com>
2022-08-30 01:31:48 -0400

    Refactor code to appear on html

--- janken.js
+++ janken.js
@@ -1,78 +1,98 @@
-
 // Generates random number between 0 - 2 to represent rock, paper, and scissor
 function getComputerChoice() {
     const options = ['rock', 'paper', 'scissor'];
     let choice = Math.floor(Math.random() * 3);
+    document.querySelector('.compChoice').innerText = `Dr.Fluffball chooses ${options[choice].toUpperCase()} !!`;
+
     return options[choice];
 }
 
-// User prompt will determine choice
-function getPlayerChoice() {
-    let choice = prompt("Dr.Evel & his minions has challenge you to a battle! Pick your weapon: rock, paper, or scissor?").toLowerCase()
+function getPlayerChoice(e) {
+    let choice = e.target.id;
+    document.querySelector('.playerChoice').innerText = `You choose ${choice.toUpperCase()}!!`
 
-    while(choice != "rock" && choice != "paper" && choice != "scissor") {
-        alert("Not a valid choice, try again.")
-        choice = prompt("Rock, Paper, or Scissor?").toLowerCase()
-    }  
     return choice;
 }
 
-let player_score = 0;
-let computer_score = 0;
+let playerWins = 0;
+let computerWins = 0;
 
 // decides the winner base on the two choices 
-function playRound(computerChoice, playerChoice) {
-    console.log("Dr.Evel chooses: " + computerChoice);
+function playRound(e) {
+    let playerChoice = getPlayerChoice(e);
+    let computerChoice = getComputerChoice();
+
+    let result = document.querySelector('.result');
+    let playerScore = document.querySelector('.playerScore');
+    let compScore = document.querySelector('.compScore');
+
     switch (computerChoice) {
         case 'rock':
             if (playerChoice == 'rock') {
-                return "It's a Tie!";
+                result.innerText =  "It's a Tie!";
             } else if (playerChoice == 'paper')  {
-                player_score++;
-                return "You Win! Paper beats Rock";
+                playerWins++;
+                result.innerText = "You Win! Paper beats Rock";
             } else {
-                computer_score++;
-                return "You Lose! Rock beats Scissor";
+                computerWins++;
+                result.innerText = "You Lose! Rock beats Scissor";
             }
+            break;
         case 'paper':
-            if (playerChoice == 'rock') {
-                computer_score++;
-                return "You Lose! Paper beats Rock";
-            } else if (playerChoice == 'paper')  {
-                return "It's a Tie!";
+            if (playerChoice == 'paper') {
+                result.innerText = "It's a Tie!";
+            } else if (playerChoice == 'scissor')  {
+                playerWins++;
+                result.innerText = "You Win! Scissor beats Paper";
             } else {
-                player_score++;
-                return "You Win! Scissor beats Paper";
+                computerWins++;
+                result.innerText = "You Lose! Paper beats Rock";
             }
+            break;
         case 'scissor':
-            if (playerChoice == 'rock') {
-                player_score++;
-                return "You Win! Rock beats Scissor";
-            } else if (playerChoice == 'paper')  {
-                computer_score++;
-                return "You Lose! Scissor beats Paper";
+            if (playerChoice == 'scissor') {
+                result.innerText = "It's a Tie!";
+            } else if (playerChoice == 'rock')  {
+                playerWins++;
+                result.innerText = "You Win! Rock beats Scissor";
             } else {
-                return "It's a Tie!";
+                computerWins++;
+                result.innerText = "You Lose! Scissor beats Paper";
             }
-        default :
-            return "Not sure what happened..."
+            break;
+    }
+
+    playerScore.innerText = playerWins;
+    compScore.innerText = computerWins;
+    
+    if(playerWins == 5 || computerWins == 5) {
+        if(playerWins == 5) {
+            result.innerText = "YOU WON! The match fills you with determination to keep moving forward!"
+        } else {
+            result.innerText = "Dr.Fluffball WINS! Just like any setback like in life, we must keep moving forward!"
+        }
     }
 }
 
 //Plays 5 rounds to determine the winner & keeps tab of score
 function game() {
-    for(let i = 0; i < 5; i++) {
-        console.log("~SCORE~ You: " + player_score +  " Dr.Evel: " + computer_score);
-        console.log(playRound(getComputerChoice(), getPlayerChoice()));
-    }
+    // for(let i = 0; i < 5; i++) {
+    //     console.log("~SCORE~ You: " + player_score +  " Dr.Fluffball: " + computer_score);
+    //     console.log(playRound(getComputerChoice(), getPlayerChoice()));
+    // }
+    // while(playerWins != 5 || computerWins != 5) {
+        let buttons = Array.from(document.querySelectorAll('button'));
+        buttons.forEach(button => button.addEventListener('click', playRound));
+    // }
 
-    if (player_score < computer_score) {
-        console.log("Dr.Evel & his minions have won! We'll get em next time!");
-    } else if (player_score > computer_score) {
-        console.log("You triumph over Dr.Evel & his minions. Huzzah!!");
-    } else {
-        console.log("Stalemate! A worthy opponent indeed, your next battle will be legendary!");
-    }
+
+    // if (playerWins < computerWins) {
+    //     console.log("Dr.Fluffball & his minions have won! We'll get em next time!");
+    // } else if (playerWins > computerWins) {
+    //     console.log("You triumph over Dr.Fluffball & his minions. Huzzah!!");
+    // } else {
+    //     console.log("Stalemate! A worthy opponent indeed, your next battle will be legendary!");
+    // }
 }
 
 game();
